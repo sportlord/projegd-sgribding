80947a9e5b1f0142009dc288d619665994421722
Sportlord <schrammeropxd@gmail.com>
2021-06-01 21:32:02 +0200

    Mehr fungtionalidäd

--- src/common.h
+++ src/common.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
 #include <cstdint>
 #include <vector>
 #include <array>
-#include <Windows.h>
-#include <WinUser.h>
+#include <windows.h>
+#include <winuser.h>
 
 #include "random.h"
 
@@ -33,6 +35,7 @@ struct input_config {
     input_key_config kc{};
     input_mouse_config mc{};
     float chance_key = .8;
+    bool simulate = false;
     string title;
 };
 
@@ -44,10 +47,16 @@ struct screen_config {
     long x, y, w, h;
 };
 
+struct output_config {
+    bool verbose = false;
+    ofstream *log_file;
+};
+
 struct config {
-    input_config ic{};
-    time_config tc{};
-    screen_config sc{};
+    input_config ic;
+    time_config tc;
+    screen_config sc;
+    output_config oc;
 };
 
 void print_key_config(input_key_config kc) {
@@ -95,11 +104,25 @@ void print_screen_config(screen_config sc) {
     cout << "Active screen " << sc.w << "x" << sc.h << " pixels" << endl;
 }
 
-void print_config(config c) {
+void print_config(config *c) {
     cout << "CONFIG" << endl;
-    print_input_config(c.ic);
-    print_time_config(c.tc);
-    print_screen_config(c.sc);
+    print_input_config(c->ic);
+    print_time_config(c->tc);
+    print_screen_config(c->sc);
+}
+
+void log(config *c, string text) {
+    if (c->oc.verbose) {
+        cout << text;
+    }
+    if (c->oc.log_file->is_open()) {
+        *c->oc.log_file << text;
+        c->oc.log_file->flush();
+    }
+}
+
+void log(config *c, stringstream *text) {
+    log(c, text->str());
 }
 
 RECT GetDesktopResolution() {
@@ -114,34 +137,37 @@ screen_config get_screen_config() {
     return { dims.left, dims.top, dims.right - dims.left, dims.bottom - dims.top };
 }
 
-bool send_input(input_config ic) {
+bool send_input(config *c) {
+    stringstream ss;
     INPUT inputs[2] = {0};
     bool need_second = true;
-    if (randf() < ic.chance_key) {
+
+    if (randf() < c->ic.chance_key) {
         // send key
         inputs[0].type = INPUT_KEYBOARD;
         inputs[0].ki.dwFlags = 0;
-        uint8_t key = 0;
+        uint8_t key;
         uint8_t idx;
-        if (randf() < ic.kc.chance_special) {
-            idx = (uint8_t) randl(0, (int) ic.kc.special.size());
-            key = ic.kc.special[idx];
+        if (randf() < c->ic.kc.chance_special) {
+            idx = (uint8_t) randl(0, (int) c->ic.kc.special.size());
+            key = c->ic.kc.special[idx];
         } else {
-            idx = (uint8_t) randl(0, (int) ic.kc.basic.size());
-            key = ic.kc.basic[idx];
+            idx = (uint8_t) randl(0, (int) c->ic.kc.basic.size());
+            key = c->ic.kc.basic[idx];
         }
-#if DEBUG
-        cout << "Sending key ";
+
+        ss << "Schigge dasde ";
         if ('A' <= key && key <= 'Z') {
-             cout << key;
+             ss << key;
         } else {
-            cout << hex << key + 0;
+            ss << hex << key + 0;
         }
-        cout << endl;
-#endif
+        ss << endl;
+        log(c, &ss);
+
         inputs[0].ki.wVk = key;
         inputs[0].ki.wScan = MapVirtualKeyA(key, MAPVK_VK_TO_VSC);
-        if (ic.kc.wScan) {
+        if (c->ic.kc.wScan) {
             inputs[0].ki.dwFlags |= KEYEVENTF_SCANCODE;
         }
         memcpy(&inputs[1], &inputs[0], sizeof(INPUT));
@@ -149,8 +175,8 @@ bool send_input(input_config ic) {
     } else {
         // send mouse
         inputs[0].type = INPUT_MOUSE;
-        int option_count = ic.mc.move + ic.mc.left + ic.mc.right;
-        uint8_t options = ic.mc.move << 0 |  ic.mc.left << 1 | ic.mc.right << 2;
+        int option_count = c->ic.mc.move + c->ic.mc.left + c->ic.mc.right;
+        uint8_t options = c->ic.mc.move << 0 | c->ic.mc.left << 1 | c->ic.mc.right << 2;
         int option;
         do {
             option = (int) round(randf() * (float) option_count);
@@ -159,34 +185,34 @@ bool send_input(input_config ic) {
             need_second = false;
             inputs[0].mi.dwFlags = MOUSEEVENTF_MOVE;
             long x, y;
-            if (ic.mc.relative) {
-                inputs[0].mi.dx = (x = randl(-ic.mc.maxX, ic.mc.maxX));
-                inputs[0].mi.dy = (y = randl(-ic.mc.maxY, ic.mc.maxY));
+            if (c->ic.mc.relative) {
+                inputs[0].mi.dx = (x = randl(-c->ic.mc.maxX, c->ic.mc.maxX));
+                inputs[0].mi.dy = (y = randl(-c->ic.mc.maxY, c->ic.mc.maxY));
             } else {
                 screen_config sc = get_screen_config();
                 inputs[0].mi.dwFlags |= MOUSEEVENTF_ABSOLUTE;
                 inputs[0].mi.dx = MulDiv((x = randl(0, sc.w)), 65536, sc.w);
                 inputs[0].mi.dy = MulDiv((y = randl(0, sc.h)), 65536, sc.h);
             }
-#if DEBUG
-            cout << "Moving mouse ";
-            if (ic.mc.relative) {
-                cout << "relatively by ";
+
+            ss << "Bewegne die Maus ";
+            if (c->ic.mc.relative) {
+                ss << "um ";
             } else {
-                cout << "to ";
+                ss << "auf ";
             }
-            cout << x << ", " << y << endl;
-#endif
+            ss << x << ", " << y << endl;
+            log(c, &ss);
         } else {
-#if DEBUG
-            cout << "Pressing the ";
+            ss << "Drügge den ";
             if (option == 1) {
-                cout << "left";
+                ss << "lingen";
             } else {
-                cout << "right";
+                ss << "rechten";
             }
-            cout << " mouse button" << endl;
-#endif
+            ss << " Mausknof" << endl;
+            log(c, &ss);
+
             if (option == 1) {
                 inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
                 memcpy(&inputs[1], &inputs[0], sizeof(INPUT));
@@ -198,24 +224,26 @@ bool send_input(input_config ic) {
             }
         }
     }
-#if !SIMULATE
-    INPUT send[1] = {};
-    send[0] = inputs[0];
-    if (SendInput(1, send, sizeof(INPUT)) != 1) return false;
-    if (need_second) {
-        Sleep(50); // Tiny sleep for Window to register event
-        send[0] = inputs[1];
+
+    if (!c->ic.simulate) {
+        INPUT send[1] = {};
+        send[0] = inputs[0];
         if (SendInput(1, send, sizeof(INPUT)) != 1) return false;
+        if (need_second) {
+            Sleep(50); // Tiny sleep for Window to register event
+            send[0] = inputs[1];
+            if (SendInput(1, send, sizeof(INPUT)) != 1) return false;
+        }
     }
-#endif
+
     return true;
 }
 
-void delay_random(time_config tc) {
-    uint16_t delta = abs(tc.delay[1] - tc.delay[0]);
-    auto delay = min(tc.delay[0], tc.delay[1]) * 1000 + (uint16_t) round((double) randf() * (double) delta * 1000.);
-#if DEBUG
-    cout << "Sleeping " << dec << delay + 0 << "ms" << endl;
-#endif
+void delay_random(config *c) {
+    uint16_t delta = abs(c->tc.delay[1] - c->tc.delay[0]);
+    auto delay = min(c->tc.delay[0], c->tc.delay[1]) * 1000 + (uint16_t) round((double) randf() * (double) delta * 1000.);
+    stringstream ss;
+    ss << "Wadde " << dec << delay + 0 << "ms" << endl;
+    log(c, &ss);
     Sleep(delay);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+#include <fstream>
 #include <Windows.h>
 #include <synchapi.h>
 #include <string>
 #include <sstream>
 
 #define DEBUG 1
-#define SIMULATE 0
-
+#include "../include/argparse/argparse.h"
 #include "./games/lol.h"
 
 using namespace std;
@@ -25,59 +25,94 @@ uint16_t get_uint16_or_default(uint16_t def) {
     return parsed;
 }
 
-bool is_game_active(config c) {
+bool is_game_active(config *c) {
     HWND window = GetForegroundWindow();
     char title[255];
     GetWindowTextA(window, title, 254);
-    return c.ic.title.compare(title) == 0;
+    return c->ic.title == title;
 }
 
-int main(VOID) {
+int main(int argc, const char* argv[]) {
 //    ghSemaphore = CreateSemaphore(nullptr, 1, 1, nullptr);
 //
 //    if (ghSemaphore == nullptr) {
 //        printf("CreateSemaphore error: %lu\n", GetLastError());
 //        return 1;
 //    }
+    argparse::ArgumentParser parser("Projegd Sgribding", "Varieirde wariande eines guden Brograms");
+    parser.add_argument("game", "Des Spil des ihr etzala spild", true).count(1).position(0);
+    parser.add_argument("-l", "--delay-lower", "Midesdens so lange wadden bis was basirt (min: 5, max: 120)", false).count(1);
+    parser.add_argument("-u", "--delay-upper", "Magsimal so lange wadden bis was basirt (max: 300)",false).count(1);
+    parser.add_argument("-f", "--file", "Schreib die Ausgabne in ein Buch",false).count(1);
+    parser.add_argument("-v", "--verbose", "Erweiderde ausgabne",false);
+    parser.add_argument("-s", "--simulate", "Agdionen nur sumulirn",false);
+    parser.enable_help();
+    auto err = parser.parse(argc, argv);
+    if (err) {
+        cout << err << endl;
+        return -1;
+    }
 
-    cout << "Minimum seconds delay (default: 15, min: 5, max 120): ";
+    if (parser.exists("help")) {
+        parser.print_help();
+        return 0;
+    }
 
-    uint16_t delayMin = get_uint16_or_default(15);
-    delayMin = min(120, max(5, delayMin));
-    cout << "Maximum seconds delay (default: 45, max 300): ";
-    uint16_t delayMax = get_uint16_or_default(45);
-    delayMax = min(300, max(delayMin, delayMax));
+    string game = parser.get<string>("game");
+    input_config ic;
+    if (game == "lol") {
+        ic = create_config_lol();
+    } else {
+        cout << "Des Schpil kenn ich net" << endl;
+        return -2;
+    }
 
-    cout << "Currently only league is supported..." << endl;
+    time_config tc = { { 15, 45 } };
+    if (parser.exists("delay-lower")) {
+        tc.delay[0] = min(120, max(5, parser.get<uint16_t>("delay-lower")));
+    }
+    if (parser.exists("delay-lower")) {
+        tc.delay[1] = min(300, max(tc.delay[0], parser.get<uint16_t>("delay-upper")));
+    }
 
-    config config = {
-            create_config_lol(),
-            {
-                    {delayMin, delayMax}
-            },
-            get_screen_config()
-    };
+    ofstream log_file;
+    if (parser.exists("file")) {
+        log_file.open(parser.get<string>("file"), ios::out | ios::trunc);
+    }
 
+    ic.simulate = parser.exists("simulate");
+
+    config c = {ic, tc, get_screen_config(), { parser.exists("verbose"), &log_file } };
 
     char show_conf[2]; // Need 2 bytes as cin stores newlines
-    cout << "Should I show you the current config? [Ny] ";
+    cout << "Wilsd du noch die Gonfiguration sehne? [Ny] ";
     cin.get(show_conf, 2);
     switch (show_conf[0]) {
         case 'y':
         case 'Y':
-            print_config(config);
+            print_config(&c);
             break;
     }
 
-    screen_config sc = get_screen_config();
     for (;;) {
-        if (paused || !is_game_active(config)) {
+        if (paused) {
             Sleep(500);
             continue;
         }
-        if (!send_input(config.ic))
+        if (!is_game_active(&c)) {
+            log(&c, "Net im Spil. Ich wadde...");
+            while (!is_game_active(&c)) {
+                Sleep(500);
+            }
+            log(&c, "Weida\n");
+        }
+        if (!send_input(&c))
             break;
-        delay_random(config.tc);
+        delay_random(&c);
+    }
+
+    if (log_file.is_open()) {
+        log_file.close();
     }
 
 //    CloseHandle(ghSemaphore);
